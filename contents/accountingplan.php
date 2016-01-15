237f049198d9e234369a8f085587bc9f77019a15
perrick <perrick@d07dd117-7d15-4c26-aaaa-e67aa2e0e813>
2016-01-15 14:33:55 +0000

    Simplification de l'affichage
    git-svn-id: svn://svn.noparking.net/usr/local/svn/lozeil@142 d07dd117-7d15-4c26-aaaa-e67aa2e0e813

--- contents/accountingplan.php
+++ contents/accountingplan.php
@@ -1,5 +1,5 @@
 <?php
-/* Lozeil -- Copyright (C) No Parking 2013 - 2013 */
+/* Lozeil -- Copyright (C) No Parking 2013 - 2016 */
 
 $menu = Plugins::factory("Menu_Area");
 echo $menu->show();
@@ -10,11 +10,10 @@ echo $heading->show();
 $accounting_codes = new Accounting_Codes();
 $accounting_codes->select();
 
-$working = $accounting_codes->display();
-$area = new Working_Area($working);
+$area = new Working_Area($accounting_codes->display());
 
 $submitimport = new Html_input("import_default",__("import default account plan"), "submit");
 $form = "<br><center>".$submitimport->input()."</center><script> var message_confirm = \"".__('are you sure? the previous accounting plan will be deleted')."\" ;</script>";
 
 echo $area->show();
-echo $form."<br><br>";
+echo $form;
